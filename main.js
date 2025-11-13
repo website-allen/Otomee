2967064a274217d575e5850d724b0798e45a90ac
Allen <70351240+website-allen@users.noreply.github.com>
2025-11-13 14:06:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -147,6 +147,6 @@ var portfolioSwiper = new Swiper('.portfolio-details-slider.swiper', {
   speed: 800, // smooth transition speed
 });
 /*===Portfolio details slider end===*/
-alert('js is running 13:54pm');
+alert('js is running 14:05pm');
 
 /*========END OF FILE=======*/
