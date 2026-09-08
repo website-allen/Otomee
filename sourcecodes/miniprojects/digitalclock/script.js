6a9be34c63a8b8f3e65a2c9a6e97aa0571e21385
Allen <70351240+website-allen@users.noreply.github.com>
2026-09-08 11:13:33 +0100

    Add files via upload

---
+++ sourcecodes/miniprojects/digitalclock/script.js
@@ -0,0 +1,16 @@
+function updateClock() {
+	const now = new Date();
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+	
+	hours = hours < 10 ? '0' + hours : hours;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+	
+	const timeString = `${hours}:${minutes}:${seconds}`;
+	document.getElementById("digital-clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
